64c3650bc4b2a8c57167e53dd10218c6db6a5cd1
KlasterK <kadakinvi2@gmail.com>
2025-12-01 17:27:04 +0900

    Add proper testing

--- test/getblock.cpp
+++ test/getblock.cpp
@@ -1,13 +1,27 @@
 #include "minecraft.h"
 #include <iostream>
 
+constexpr int GOLD_BLOCK = 41;
+
 int main()
 {
+    int error_state{};
     cmpi_connection_t conn;
-    cmpi_net_connect_default(&conn);
 
-    cmpi_set_block(&conn, 2, 66, -83, 41); // golden block
-    // std::cout << "Block @(0; 50; 0) " << cmpi_get_block(&conn, 0, 50, 0) << std::endl;
+    cmpi_net_connect_default(&conn);
+    std::cout << "cmpi_net_connect_default : is_connected=" << conn.is_connected
+              << ", error_state=" << conn.error_state << std::endl;
+    error_state |= conn.error_state;
+
+    cmpi_set_block(&conn, 2, 66, -83, GOLD_BLOCK);
+    std::cout << "cmpi_set_block : is_connected=" << conn.is_connected
+              << ", error_state=" << conn.error_state << std::endl;
+    error_state |= conn.error_state;
 
     cmpi_net_close(&conn);
+    std::cout << "cmpi_net_close : is_connected=" << conn.is_connected
+              << ", error_state=" << conn.error_state << std::endl;
+    error_state |= conn.error_state;
+
+    return error_state;
 }
