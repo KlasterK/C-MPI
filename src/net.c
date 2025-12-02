36909d421101a1678fbbe3394c6fec27038a6110
KlasterK <kadakinvi2@gmail.com>
2025-12-02 23:44:00 +0900

    Improve net code

--- src/net.c
+++ src/net.c
@@ -8,23 +8,6 @@
 #define _CMPI_BUF_MIN_DFL_CAP 1024
 
 
-static void _ensure_buf_capacity(cmpi_buffer_t *buf, size_t needed)
-{
-    if(buf->len + needed <= buf->cap)
-        return;
-    
-    size_t new_cap = buf->cap == 0 
-                   ? _CMPI_BUF_MIN_DFL_CAP 
-                   : buf->cap * 2;
-    
-    while(new_cap < buf->len + needed)
-        new_cap *= 2;
-    
-    buf->data = realloc(buf->data, new_cap);
-    buf->cap = new_cap;
-}
-
-
 static void _init_buf(cmpi_buffer_t *buf)
 {
     buf->data = NULL;
@@ -40,28 +23,68 @@ static void _cleanup_buf(cmpi_buffer_t *buf)
         free(buf->data);
         buf->data = NULL;
     }
+    
+    buf->len = 0;
+    buf->cap = 0;
+}
+
+
+static void _ensure_buf_capacity(cmpi_buffer_t *buf, size_t needed)
+{
+    if(buf->len + needed <= buf->cap)
+        return;
+    
+    size_t new_cap = buf->cap == 0 
+                   ? _CMPI_BUF_MIN_DFL_CAP 
+                   : buf->cap * 2;
+    
+    while(new_cap < buf->len + needed)
+        new_cap *= 2;
+    
+    char *data = realloc(buf->data, new_cap);
+    if(data == NULL)
+    {
+        fprintf(stderr, "C-MPI: fatal: Out of Memory\n");
+        exit(1);
+    }
+    buf->data = data;
+    buf->cap = new_cap;
+}
+
+
+static void _shift_buf(cmpi_buffer_t *buf, int n)
+{
+    if(n <= 0)
+        return;
+    
+    if(n >= buf->len)
+    {
+        buf->len = 0;
+        return;
+    }
+
+    buf->len -= n;
+    memmove(buf->data, buf->data + n, buf->len);
 }
 
 
 // Wait while terminator is not present in buffer or connection is not terminated
-// Returns 0 if recved terminator, -1 on error
+// Returns received terminator, -1 on error
 static int _wait_for_input(cmpi_connection_t *conn, const char *terminators)
 {
     while(conn->is_connected && !conn->error_state)
     {
-        if(conn->input_buf.len > 0)
+        for(size_t i = 0; i < conn->input_buf.len; ++i)
         {
-            for(size_t i = 0; i < conn->input_buf.len; ++i)
-            {
-                if(strchr(terminators, conn->input_buf.data[i]))
-                    return -1;
-            }
+            int c = conn->input_buf.data[i];
+            if(strchr(terminators, c) == NULL)
+                continue;
         }
-
+        
         uv_run(&conn->loop, UV_RUN_ONCE);
     }
 
-    return 0;
+    return -1;
 }
 
 
@@ -80,10 +103,11 @@ static void _on_alloc(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf)
 static void _on_read(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf)
 {
     cmpi_connection_t *conn = stream->data;
-    if(nread > 0)
+    if(nread >= 0)
+    {
         conn->input_buf.len += nread;
-    else if(nread == 0)
         return;
+    }
     
     if(nread != UV_EOF)
     {
@@ -139,7 +163,7 @@ void cmpi_net_connect(cmpi_connection_t *conn, const char *address, int port)
     _init_buf(&conn->output_buf);
 
     struct sockaddr_in dst;
-    uv_ip4_addr(address, port, &dst);
+    uv_ip4_addr(address, port, &dst); // TODO: auto-selection of IPv4 or IPv6 using getaddrinfo
 
     uv_tcp_connect(&conn->connect_req, &conn->socket, (struct sockaddr *)&dst, _on_connect);
     while(!conn->is_connected && !conn->error_state)
@@ -242,7 +266,8 @@ int cmpi_count_separators(cmpi_connection_t *conn, int sep, int terminator)
         return -1;
     
     int count = 0;
-    for(size_t i = 0; i < conn->input_buf.len; ++i)
+    size_t i = 0;
+    for(; i < conn->input_buf.len; ++i)
     {
         if(conn->input_buf.data[i] == terminator)
             break;
@@ -250,5 +275,7 @@ int cmpi_count_separators(cmpi_connection_t *conn, int sep, int terminator)
         if(conn->input_buf.data[i] == sep)
             ++count;
     }
+
+    _shift_buf(&conn->input_buf, i+1);
     return count;
 }
--- test/setblock.cpp
+++ test/setblock.cpp
@@ -5,23 +5,31 @@ constexpr int GOLD_BLOCK = 41;
 
 int main()
 {
-    int error_state{};
     cmpi_connection_t conn;
 
     cmpi_net_connect_default(&conn);
-    std::cout << "cmpi_net_connect_default : is_connected=" << conn.is_connected
-              << ", error_state=" << conn.error_state << std::endl;
-    error_state |= conn.error_state;
+    if(conn.error_state)
+    {
+        std::cout << "cmpi_net_connect_default : is_connected=" << conn.is_connected
+                  << ", error_state=" << conn.error_state << std::endl;
+        return conn.error_state;
+    }
 
     cmpi_set_block(&conn, 2, 66, -83, GOLD_BLOCK);
-    std::cout << "cmpi_set_block : is_connected=" << conn.is_connected
-              << ", error_state=" << conn.error_state << std::endl;
-    error_state |= conn.error_state;
+    if(conn.error_state)
+    {
+        std::cout << "cmpi_set_block : is_connected=" << conn.is_connected
+                  << ", error_state=" << conn.error_state << std::endl;
+        return conn.error_state;
+    }
 
     cmpi_net_close(&conn);
-    std::cout << "cmpi_net_close : is_connected=" << conn.is_connected
-              << ", error_state=" << conn.error_state << std::endl;
-    error_state |= conn.error_state;
+    if(conn.error_state)
+    {
+        std::cout << "cmpi_net_close : is_connected=" << conn.is_connected
+                  << ", error_state=" << conn.error_state << std::endl;
+        return conn.error_state;
+    }
 
-    return error_state;
+    return 0;
 }
