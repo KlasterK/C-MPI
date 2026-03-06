6b88a065bcfaa4b491c59bbc362d70f91c29890a
KlasterK <kadakinvi2@gmail.com>
2026-03-07 02:08:42 +0900

    test: use new net.h functional in the test suite
    Now testing macros also print an error message using new cmpi_net_get_error().

--- tests/commontest.h
+++ tests/commontest.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <uv.h>
+#include "net.h"
 
 #define _TST(fn, conn, ...) \
 { \
@@ -11,12 +11,13 @@
     if((conn)->error_state) \
     { \
         printf( \
-            "%s: is_connected=%d, error_state=%d\n", \
+            "%s: is_connected=%d, error_state=%d, cmpi_net_get_error='%s'\n", \
             #fn, \
             (conn)->is_connected, \
-            (conn)->error_state \
+            (conn)->error_state, \
+            cmpi_net_get_error((conn)) \
         ); \
-        exit((conn)->error_state); \
+        exit(1); \
     } \
 }
 
@@ -26,12 +27,13 @@
     if((conn)->error_state) \
     { \
         printf( \
-            "%s: is_connected=%d, error_state=%d\n", \
+            "%s: is_connected=%d, error_state=%d, cmpi_net_get_error='%s'\n", \
             #fn, \
             (conn)->is_connected, \
-            (conn)->error_state \
+            (conn)->error_state, \
+            cmpi_net_get_error((conn)) \
         ); \
-        exit((conn)->error_state); \
+        exit(1); \
     } \
 }
 
